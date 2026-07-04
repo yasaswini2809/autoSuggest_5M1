886be9f8a0dacc51b9fc2cd7ed929d5eb6148924
yasaswini2809 <kuryasaswini2006@gmail.com>
2026-07-04 09:02:19 +0530

    day-1

--- frontend/index.js
+++ frontend/index.js
@@ -1,2 +1,24 @@
-console.log("Hello from JS")
-alert("Hello from JS")
+console.log("Hello from js")
+const users = [
+    {
+        "name": "Jane Doe",
+        "gender": "female",
+        "image": "./images/jane.png"
+    },
+    {
+        "name": "John Doe",
+        "gender": "male",
+        "image": "./images/john.png"
+    }
+];
+let curUserId = 0;
+function toggle(){
+    if(curUserId == 0){
+        curUserId = 1;
+    } else {
+        curUserId = 0;
+    }
+    document.getElementById("img").src=users[curUserId].image;
+    document.getElementById("card-name").innerText=users[curUserId].name;
+}
+alert("Hello from js")
