fb59323e877b28434d07ef28df6ec6e326aa7fef
yasaswini2809 <kuryasaswini2006@gmail.com>
2026-07-04 10:00:09 +0530

    random-user

--- frontend/index.js
+++ frontend/index.js
@@ -20,5 +20,31 @@ function toggle(){
     }
     document.getElementById("img").src=users[curUserId].image;
     document.getElementById("card-name").innerText=users[curUserId].name;
+    document.getElementById("card-gender").innerText=users[curUserId].gender;
 }
 alert("Hello from js")
+
+function getRandomUser()
+{
+  fetch("https://randomuser.me/api")
+    .then(function (data){
+      return data.json()
+    })
+    .then(function (parsedData){
+      // console.log(parsedData)
+      let gender = parsedData.results[0].gender;
+
+      let first = parsedData.results[0].name.first;
+      let last = parsedData.results[0].name.last;
+      let name = first  + " " + last;
+      let imageUrl = parsedData.results[0].picture.large;
+
+      // gender
+      document.getElementById("card-gender").innerText=gender;
+      // name
+      document.getElementById("card-name").innerText=name
+      // img
+      document.getElementById("img").src=imageUrl
+
+    })
+}
